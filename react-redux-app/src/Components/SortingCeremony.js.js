2984d3b2fe572dec1bf400a800ddee71ce9304d5
CodingCCarpenter <christineassists@aol.com>
2019-11-21 01:34:20 -0600

    working on reducer

---
+++ react-redux-app/src/Components/SortingCeremony.js.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default SortingCeremony = () => {
+    return (
+        <>
+            <div className='songParchment'>
+                <quote>
+                    <p>"Oh, you may not think I'm pretty,
+                    But don't judge on what you see,
+                    I'll eat myself if you can find
+                    A smarter hat than me.</p>
+
+                    <p>You can keep your bowlers black,
+                    Your top hats sleek and tall,
+                    For I'm the Hogwarts Sorting Hat
+                    And I can cap them all.</p>
+
+                    <p>There's nothing hidden in your head
+                    The Sorting Hat can't see,
+                    So try me on and I will tell you
+                    Where you ought to be.</p>
+
+                    <p>You might belong in <span className='houseGryffindor'>Gryffindor</span>,
+                    Where dwell the brave at heart,
+                    Their daring, nerve and chivalry,
+                    Set Gryffindors apart;</p>
+
+                    <p>You might belong in <span className='houseHufflepuff'>Hufflepuff</span>,
+                    Where they are just and loyal,
+                    Those patient Hufflepuffs are true,
+                    And unafraid of toil;</p>
+
+                    <p>Or yet in wise old <span className='houseRavenclaw'>Ravenclaw</span>,
+                    If you've a ready mind,
+                    Where those of wit and learning,
+                    Will always find their kind;</p>
+
+                    <p>Or perhaps in <span className='houseSlytherin'>Slytherin</span>,
+                    You'll make your real friends,
+                    Those cunning folk use any means,
+                    To achieve their ends.</p>
+
+                    <p>So put me on! Don't be afraid!
+                    And don't get in a flap!
+                    You're in safe hands <span>(Though I have none!)</span>
+                    For I'm a Thinking Cap!"</p>
+                </quote>
+            </div>
+        </>
+    )
+}
--- react-redux-app/src/reducers/index.js
+++ react-redux-app/src/reducers/index.js
@@ -1,5 +1,29 @@
 import {
-    POTTER_LOAD_START,
-    POTTER_LOAD_SUCCESS,
-    POTTER_LOAD_FAILURE
-}  from '../actions/index';
+    SORTINGHAT_LOAD_START,
+    SORTINGHAT_LOAD_SUCCESS,
+    SORTINGHAT_LOAD_FAILURE
+}  from '../actions/sortingActions';
+
+
+const initialState = {
+    isLoading = false,
+    error: "Someone grab the time turner! We have an error!",
+    house: "Hmmm... Better be ..."
+};
+
+const sortingReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SORTINGHAT_LOAD_START:
+            return {
+                ...state,
+                isLoading: true
+            };
+        case SORTINGHAT_LOAD_SUCCESS:
+            return {
+                ...state,
+                house: {
+                    { state.house }
+                }
+            }
+    }
+}
