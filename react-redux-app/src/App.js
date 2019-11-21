1ce4107dd0ef02fe4e926bb082f3c3f7df86694f
CodingCCarpenter <christineassists@aol.com>
2019-11-21 12:36:11 -0600

    import route to app.js, add routes to app.js

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import ReactDom from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from redux-thunk;
+import SortingCeremony from './Components/SortingCeremony.js';
 
-
+//import router from 'react-dom'
+import { Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -14,7 +15,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to Hogwarts</h1>
         <Img src= 'http://getwallpapers.com/wallpaper/full/0/6/b/15711.jpg' alt='hogwarts on foggy night' />
-        <SortingCeremony />
+        <Route exact path="/components/SortingCeremony" component={ SortingCeremony } />
       </header>
     </div>
   );
