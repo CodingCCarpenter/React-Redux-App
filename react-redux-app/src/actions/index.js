0a2c8851cf0fcd637e6915e724ae62115b39b479
CodingCCarpenter <christineassists@aol.com>
2019-11-21 00:41:48 -0600

    add axios

---
+++ react-redux-app/src/actions/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const POTTERDATA_LOAD_START = 'POTTERDATA_LOAD_START';
+export const POTTERDATA_LOAD_SUCCESS = 'POTTERDATA_LOAD_SUCCESS';
+export const POTTERDATA_LOAD_FAILURE = 'POTTERDATA_LOAD_FAILURE';
+
+export const getPotterHouseData = () => dispatch => {
+    
+}
---
+++ react-redux-app/src/reducers/index.js
@@ -0,0 +1,5 @@
+import {
+    POTTER_LOAD_START,
+    POTTER_LOAD_SUCCESS,
+    POTTER_LOAD_FAILURE
+}  from '../actions/index';
